b86799972e5bac9455c7044e07bbbefa6e914d0d
Craig Kelly <craig.n.kelly@gmail.com>
2015-10-08 17:32:38 -0500

    handle windows/vagrant/symlink combo

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cd $SCRIPT_DIR
 
 # Set up a virtual environment using python3, use the new environment, and
 # install our dependencies
-virtualenv -p python3 env
+virtualenv -p python3 env --always-copy
 source $SCRIPT_DIR/env/bin/activate
 pip install --upgrade requests[security]
 pip install -r requirements.txt
